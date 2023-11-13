d63ea7df52cc2c3de574d3d5b44ad0c58ff92322
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2023-11-13 20:36:30 +0200

    Update WalletUser.java

--- User/WalletUser.java
+++ User/WalletUser.java
@@ -3,18 +3,15 @@ import Source.*;
 public class WalletUser extends User{
     Wallet wallet;
 
-    public WalletUser(String username, String password, String name, String phoneNo, Type type) {
+    public WalletUser(String username, String password, String name, String phoneNo, Type type, Wallet wallet) {
         super(username, password, name, phoneNo, type);
+        this.wallet = wallet;
     }
 
     @Override
     public void deposit(double amount) {
 
         wallet.deposit(amount, wallet.getPhoneNo());
-    }
-    public WalletUser(String username, String password, String name, String phoneNo, Type type, Wallet wallet) {
-        super(username, password, name, phoneNo, type);
-        this.wallet = wallet;
     }
     @Override
     public double getBalance() {

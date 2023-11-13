aea28f01edc83f541ed470d98727aa86ee202963
noorshehab <nhatem1994@gmail.com>
2023-11-13 12:06:28 +0200

    packages added

--- Bills/WaterBill.java
+++ Bills/WaterBill.java
@@ -3,5 +3,4 @@ public class WaterBill extends Bill {
   @Override
   public void payBill() {
 
-  }
-}
+}}
---
+++ External/UtilAPI.java
@@ -0,0 +1,19 @@
+package External;
+
+import Bills.Bill;
+
+import java.util.Random;
+
+public class UtilAPI {
+    private UtilAcc CompanyAcc;
+    public float getAmount(string Code){
+        Random rand = new Random();
+        return rand.nextFloat(1000);
+    }
+    public User getAccount(string Code){
+        return UtilAcc;
+    }
+
+    public void BillPayed(Bill bill){
+    }
+}

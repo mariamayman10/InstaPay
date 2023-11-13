f02b5ac4d12f050d626a2c2dae2b746be077ab43
noorshehab <nhatem1994@gmail.com>
2023-11-13 12:01:30 +0200

    Added utility company api

---
+++ Bills/UtilAPI.java
@@ -0,0 +1,17 @@
+package Bills;
+
+import java.util.Random;
+
+public class UtilAPI {
+    private UtilAcc CompanyAcc;
+    public float getAmount(string Code){
+        Random rand = new Random();
+        return rand.nextFloat(1000);
+    }
+    public User getAccount(string Code){
+        return UtilAcc;
+    }
+
+    public void BillPayed(Bill bill){
+    }
+}
